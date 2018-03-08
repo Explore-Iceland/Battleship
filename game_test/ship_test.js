48336801b9ca4ccc175821ad9e30f5c5ba2bd3e9
Marcel Radix <radixmarcel@gmail.com>
2018-03-08 13:59:54 +0000

    Test Suite Part 2

--- game_test/ship_test.js
+++ game_test/ship_test.js
@@ -43,3 +43,27 @@ describe('checkForShip', function(){
     expect(checkForShip(player,[0, 0])).to.deep.equal(player.ships[0]);
     expect(checkForShip(player,[9, 9])).to.be.false;
   });
+
+  it('should handle checking multiple ships', function(){
+
+    player = {
+      ships: [
+        {
+          locations :[[0, 0], [0, 1]]
+        },
+        {
+          locations :[[1, 0], [1, 1]]
+        },
+        {
+          locations :[[2, 0], [2, 1], [2, 2], [2, 3]]
+        }
+      ]
+    };
+    expect(checkForShip(player,[0, 1])).to.deep.equal(player.ships[0]);
+    expect(checkForShip(player,[0, 0])).to.deep.equal(player.ships[0]);
+    expect(checkForShip(player,[1, 0])).to.deep.equal(player.ships[1]);
+    expect(checkForShip(player,[1, 1])).to.deep.equal(player.ships[1]);
+    expect(checkForShip(player,[2, 3])).to.deep.equal(player.ships[2]);
+    expect(checkForShip(player,[9, 9])).to.be.false;
+  });
+});
