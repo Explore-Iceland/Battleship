03b04a58523d4c5b264db0fd6cd53954292b8f47
Marcel Radix <radixmarcel@gmail.com>
2018-03-08 13:57:11 +0000

    Ship 2 and 3

--- game_test/ship_test.js
+++ game_test/ship_test.js
@@ -15,3 +15,31 @@ describe('checkForShip', function(){
 
     expect(checkForShip(player,[9,9])).to.be.false;
   });
+
+  it('should correctly report a ship located at the given coordinates', function(){
+
+    player = {
+      ships: [
+        {
+          locations :[[0, 0]]
+        }
+      ]
+    };
+
+    expect(checkForShip(player,[0,0])).to.deep.equal(player.ships[0]);
+  });
+
+  it('should handle ships located at more than one cooardinate', function(){
+
+    player = {
+      ships: [
+        {
+          locations :[[0, 0], [0, 1]]
+        }
+      ]
+    };
+
+    expect(checkForShip(player,[0, 1])).to.deep.equal(player.ships[0]);//á að vera true
+    expect(checkForShip(player,[0, 0])).to.deep.equal(player.ships[0]);
+    expect(checkForShip(player,[9, 9])).to.be.false;
+  });
