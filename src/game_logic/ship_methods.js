826fcf8f32c4b54f43997b6f5fe7f92cef0b7a3e
Ólafur Hólm <olafurholm17@gmail.com>
2018-03-09 12:05:44 +0000

    Upgrade to ES6

--- src/game_logic/ship_methods.js
+++ src/game_logic/ship_methods.js
@@ -1,32 +1,35 @@
-function checkForShip(player, coordinates){
-  var shipPresent, ship;
+/*eslint no-undef: "error"*/
+/*eslint-env node*/
 
-  for(var i = 0; i < player.ships.length; i++){
-    ship = player.ships[i];
+const checkForShip = (player, coordinates) => {
+	let shipPresent, ship
 
-    shipPresent = ship.locations.filter(function(actualCoordinate){
-      return (actualCoordinate[0]=== coordinates[0]) && (actualCoordinate[1] ===coordinates[1]);
-    })[0];
+	for(var i = 0; i < player.ships.length; i++){
+		ship = player.ships[i]
 
-    if(shipPresent){
-      return ship;
-    }
-  }
-  return false;
+		shipPresent = ship.locations.filter(function(actualCoordinate){
+			return (actualCoordinate[0]=== coordinates[0]) && (actualCoordinate[1] ===coordinates[1])
+		})[0]
+
+		if(shipPresent){
+			return ship
+		}
+	}
+	return false
 }
 
-function damageShip (ship,coordinates) {
-  ship.damage.push(coordinates);
+const damageShip = (ship,coordinates) => {
+	ship.damage.push(coordinates)
 }
 
-function fire(player, coordinates) {
-  var ship = checkForShip(player, coordinates);
+const fire = (player, coordinates) => {
+	const ship = checkForShip(player, coordinates)
 
-  if (ship) {
-    damageShip(ship, coordinates);
-  }
+	if (ship) {
+		damageShip(ship, coordinates)
+	}
 }
 
-module.exports.checkForShip = checkForShip;
-module.exports.damageShip = damageShip;
-module.exports.fire = fire;
+module.exports.checkForShip = checkForShip
+module.exports.damageShip = damageShip
+module.exports.fire = fire
