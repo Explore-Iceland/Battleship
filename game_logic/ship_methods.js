4b53522d6e285428c6dacce19f2d715e48add425
Ólafur Hólm <olafurholm17@gmail.com>
2018-03-09 09:59:44 +0000

    Fix

--- game_logic/ship_methods.js
+++ game_logic/ship_methods.js
@@ -9,7 +9,7 @@ function checkForShip(player, coordinates){
     })[0];
 
     if(shipPresent){
-      return true;
+      return ship;
     }
   }
   return false;
@@ -19,12 +19,12 @@ function damageShip (ship,coordinates) {
   ship.damage.push(coordinates);
 }
 
-function fire (player, coordinates) {
-	var ship = checkForShip(player, coordinates);
+function fire(player, coordinates) {
+  var ship = checkForShip(player, coordinates);
 
-	if (ship) {
-		damageShip(ship, coordinates);
-	}	
+  if (ship) {
+    damageShip(ship, coordinates);
+  }
 }
 
 module.exports.checkForShip = checkForShip;
