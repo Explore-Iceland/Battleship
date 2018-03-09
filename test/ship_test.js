309538d6af92f0692a9f1d056fd40ba497e8e4b0
Ólafur Hólm <olafurholm17@gmail.com>
2018-03-09 09:38:45 +0000

    Fix Elsa's test

--- test/ship_test.js
+++ test/ship_test.js
@@ -1,70 +1,70 @@
 var expect = require('chai').expect;
 
-describe('checkForShip', function(){
-  var checkForShip = require('../game_logic/ship_methods').checkForShip;
-
-  it('should correctly report no ship at a given players coordinates', function(){
-
-    player = {
-      ships: [
-        {
-          locations :[[0, 0]]
-        }
-      ]
-    };
-
-    expect(checkForShip(player,[9,9])).to.be.false;
-  });
-
-  it('should correctly report a ship located at the given coordinates', function(){
-
-    player = {
-      ships: [
-        {
-          locations :[[0, 0]]
-        }
-      ]
-    };
-
-    expect(checkForShip(player,[0,0])).to.deep.equal(player.ships[0]);
-  });
-
-  it('should handle ships located at more than one cooardinate', function(){
-
-    player = {
-      ships: [
-        {
-          locations :[[0, 0], [0, 1]]
-        }
-      ]
-    };
-
-    expect(checkForShip(player,[0, 1])).to.deep.equal(player.ships[0]);//á að vera true
-    expect(checkForShip(player,[0, 0])).to.deep.equal(player.ships[0]);
-    expect(checkForShip(player,[9, 9])).to.be.false;
-  });
-
-  it('should handle checking multiple ships', function(){
-
-    player = {
-      ships: [
-        {
-          locations :[[0, 0], [0, 1]]
-        },
-        {
-          locations :[[1, 0], [1, 1]]
-        },
-        {
-          locations :[[2, 0], [2, 1], [2, 2], [2, 3]]
-        }
-      ]
-    };
-    expect(checkForShip(player,[0, 1])).to.deep.equal(player.ships[0]);
-    expect(checkForShip(player,[0, 0])).to.deep.equal(player.ships[0]);
-    expect(checkForShip(player,[1, 0])).to.deep.equal(player.ships[1]);
-    expect(checkForShip(player,[1, 1])).to.deep.equal(player.ships[1]);
-    expect(checkForShip(player,[2, 3])).to.deep.equal(player.ships[2]);
-    expect(checkForShip(player,[9, 9])).to.be.false;
-  });
+describe('checkForShip', function () {
+	var checkForShip = require('../game_logic/ship_methods').checkForShip;
+	
+	it('should correctly report no ship at a given players coordinate', function () {
+		
+		player = {
+			ships: [
+				{
+					locations: [[0, 0]]
+				}
+			]
+		};
+		
+		expect(checkForShip(player, [9, 9])).to.be.false;
+	});
+	
+	it('should correctly report a ship located at the given coordinates', function () {
+		
+		player = {
+			ships: [
+				{
+					locations: [[0, 0]]
+				}
+			]
+		};
+		
+		expect(checkForShip(player, [0, 0])).to.be.true;
+	});
+	
+	it('should handle ships located at more than one coordinate', function () {
+		
+		player = {
+			ships: [
+				{
+					locations: [[0, 0], [0, 1]]
+				}
+			]
+		};
+		
+		expect(checkForShip(player, [0, 1])).to.be.true;
+		expect(checkForShip(player, [0, 0])).to.be.true;			
+		expect(checkForShip(player, [9, 9])).to.be.false;
+	});
+	
+	it('should handle checking multiple ships', function () {
+		
+		player = {
+			ships: [
+				{
+					locations: [[0, 0], [0, 1]]
+				},
+				{
+					locations: [[1, 0], [1, 1]]
+				},
+				{
+					locations: [[2, 0], [2, 1], [2, 2], [2, 3]]
+				}
+			]
+		};
+		
+		expect(checkForShip(player, [0, 1])).to.be.true;
+		expect(checkForShip(player, [0, 0])).to.be.true;	
+		expect(checkForShip(player, [1, 0])).to.be.true;	
+		expect(checkForShip(player, [1, 1])).to.be.true;
+		expect(checkForShip(player, [2, 3])).to.be.true;	
+		expect(checkForShip(player, [9, 9])).to.be.false;
+	});
 });
-
