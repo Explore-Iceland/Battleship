6e88057227ba52af73cd525196e6fc0d882f0805
Jónína <hansen.jonina@gmail.com>
2018-03-09 09:54:20 +0000

    lock file issue

--- test/ship_test.js
+++ test/ship_test.js
@@ -84,3 +84,34 @@ describe("damageShip", function (){
    expect(ship.damage[0]).to.deep.equal([0,0]);
  });
 });
+describe('fire', function () {
+	var fire = require('../game_logic/ship_methods').fire;
+
+	it('should record damage on the given players ship at a given cooardinate', function () {
+		var player = {
+			ships: [
+				{
+					locations: [[0, 0]],
+					damage: []
+				}
+			]
+		};
+
+		fire(player, [0, 0]);
+		expect(player.ships[0].damage[0]).to.deep.equal([0, 0]);
+	});
+
+	it('should NOT record damage if there is no ship at my cooardinate', function () {
+		var player = {
+			ships: [
+				{
+					locations: [[9, 9]],
+					damage: []
+				}
+			]
+		};
+
+		fire(player, [0, 0]);
+		expect(player.ships[0].damage).to.be.empty;
+	});
+});
